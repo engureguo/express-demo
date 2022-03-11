b19bac6b7cae6e4747075ac46df77cd69c3ca90e
engureguo <57861661+engureguo@users.noreply.github.com>
2022-03-11 15:32:29 +0800

    add about view & router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    links: [
+      { url: '/users', name: '用户' },
+      { url: '/about', name: '关于' },
+    ],
+  })
+})
 
-module.exports = router;
+module.exports = router
